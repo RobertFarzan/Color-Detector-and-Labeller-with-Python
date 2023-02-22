ce929e81db617e59952b4536ff43ae7616b1b501
Robert Farzan <44211717+RobertFarzan@users.noreply.github.com>
2023-02-22 20:42:22 +0100

    1-nn euclidean distance method

--- color.py
+++ color.py
@@ -1,19 +1,17 @@
 import pandas as pd
+import numpy as np
 
 class Colorname():
 
     def __init__(self):
         self.dataset = pd.read_csv('colorhexa_com.csv', header=0)
         self.dataset = self.dataset.rename(columns={'Red (8 bit)': 'R', 'Blue (8 bit)': 'B', 'Green (8 bit)': 'G'})
+        self.color_list = self.dataset[["R", "G", "B"]].to_numpy()
 
     def get_colorname(self, r, g, b):
-        mini = (float('inf'), '')
+        target_color = np.array([r, g, b])
+        dist = np.linalg.norm(self.color_list - target_color, axis=1, ord=2) # euclidean distance
 
-        for elem in self.dataset.iterrows():
-            elem = elem[1]
-            dist = abs(r - elem.loc['R']) + abs(g - elem.loc['G']) + abs(b - elem.loc['B'])
-            mini = min(mini, (dist, elem['Name']), key= lambda t: t[0])
-            
-            if mini[0] == 0:
-                break
-        return mini[1]
+        idx = dist.argmin()
+
+        return self.dataset.loc[idx, 'Name'] 
